f0901ff1a5b2e9ee9f7c2e921f4eed3aef307f21
James Shelton <jshelton140@googlemai.com>
2020-10-29 16:54:37 +0000

    Add command words bundle. Incorporated into GameText and CommandFactory
    Commands can now be generated from text input in locale format.

--- src/zuul/GameText.java
+++ src/zuul/GameText.java
@@ -1,7 +1,9 @@
 package zuul;
 
+import java.text.Collator;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 /**
  * Structure for helper class from Utku Özdemir on stack overflow:
@@ -10,12 +12,26 @@ import java.util.ResourceBundle;
 
 public class GameText {
     private static Locale locale;
+    private static Collator localeCollator;
 
     public static void setLocale(Locale locale) {
         GameText.locale = locale;
+        GameText.localeCollator = Collator.getInstance(locale);
     }
 
     public static String getString(String key) {
-        return ResourceBundle.getBundle("zuul.TextBundle", locale).getString(key);
+        return ResourceBundle.getBundle("zuul.GameTextBundle", locale).getString(key);
+    }
+
+    public static String getString(String bundle, String key) {
+        return ResourceBundle.getBundle("zuul."+bundle, locale).getString(key);
+    }
+
+    public static Set<String> getCommandWords() {
+        return ResourceBundle.getBundle("zuul.CommandWordsBundle", locale).keySet();
+    }
+
+    public static Collator getCollator() {
+        return GameText.localeCollator;
     }
 }
