b22cab53c0d62fa0db809f0f31b60180d5f7c4ad
James Shelton <jshelton140@googlemai.com>
2020-10-29 15:07:08 +0000

    Created GameText helper class that holds the locale and generates resourcebundles and gets strings when requested.

---
+++ src/zuul/GameText.java
@@ -0,0 +1,21 @@
+package zuul;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Structure for helper class from Utku Özdemir on stack overflow:
+ * https://stackoverflow.com/questions/19686476/java-internationalization-do-i-have-to-load-a-resource-bundle-for-every-class
+ */
+
+public class GameText {
+    private static Locale locale;
+
+    public static void setLocale(Locale locale) {
+        GameText.locale = locale;
+    }
+
+    public static String getString(String key) {
+        return ResourceBundle.getBundle("TextBundle", locale).getString(key);
+    }
+}
--- src/zuul/Main.java
+++ src/zuul/Main.java
@@ -3,6 +3,8 @@ package zuul;/*
  * and open the template in the editor.
  */
 
+import java.util.Locale;
+
 /**
  *
  * @author rej
@@ -13,6 +15,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        /**
+         * Set the locale for this instance of Zuul. With default "en, US" if no locale specified on program start.
+         *
+         * Arg check from java i18n trail: https://docs.oracle.com/javase/tutorial/i18n/intro/after.html
+         */
+        String language;
+        String country;
+
+        if (args.length != 2) {
+            language = new String("en");
+            country = new String("US");
+        } else {
+            language = new String(args[0]);
+            country = new String(args[1]);
+        }
+
+        GameText.setLocale(new Locale(language, country));
+
         Game.getInstance().play();
     }
 }
