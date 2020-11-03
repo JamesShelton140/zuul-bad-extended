71608d42f6a46a856d49de7879f45626b695e30f
James Shelton <jshelton140@googlemai.com>
2020-11-03 12:04:36 +0000

    GameInterface JavaDoc

--- src/zuul/GameInterface.java
+++ src/zuul/GameInterface.java
@@ -3,23 +3,33 @@ package zuul;
 import zuul.io.userInterfaces.UserInterface;
 
 /**
- * Enables static access to the active UserInterface.
+ * The interface to be used for the "World of Zuul" application.
+ * <p>
+ * Enables static access to the active {@link UserInterface}.
  * For example: {@link zuul.io.userInterfaces.CommandLineInterface CommandLineInterface}
  *
  * @author Timothy Shelton
- * @version 02/11/2020
  */
-
 public class GameInterface {
     /**
-     * The currently active user interface.
+     * The currently active {@link UserInterface}.
      */
     private static UserInterface userInterface;
 
+    /**
+     * Sets the active {@link UserInterface} for the application.
+     *
+     * @param userInterface  the UserInterface to set to the application, not null
+     */
     public static void set(UserInterface userInterface) {
         GameInterface.userInterface = userInterface;
     }
 
+    /**
+     * Gets the currently active {@link UserInterface}.
+     *
+     * @return the currently active user interface, not null
+     */
     public static UserInterface get() {
         return userInterface;
     }
