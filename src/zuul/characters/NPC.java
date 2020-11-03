97f047422ee60a1861565bef476bbff48e74c1a0
James Shelton <jshelton140@googlemai.com>
2020-11-03 14:50:30 +0000

    NPC JavaDoc

--- src/zuul/characters/NPC.java
+++ src/zuul/characters/NPC.java
@@ -1,13 +1,27 @@
 package zuul.characters;
 
 import zuul.Character;
-import zuul.Command;
-import zuul.Game;
 import zuul.Room;
 
+/**
+ * An AI-controlled {@link Character} for the "World of Zuul" application.
+ * <p>
+ * This class is a skeleton. It implements no behaviour.
+ * <p>
+ * There are comments suggesting how the {@link #act()} method could be implemented using an "npcBehaviour" class
+ * that represents the algorithm that determines how the NPC will act.
+ *
+ * @author Timothy Shelton
+ */
 public class NPC extends Character {
     //private npcBehaviour = behaviour;
 
+    /**
+     * Constructor
+     *
+     * @param name  the name of this NPC
+     * @param startingRoom  the {@link Room} this NPC will start in
+     */
     public NPC(String name, Room startingRoom) {
         super(name, startingRoom);
     }
