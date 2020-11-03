7363125b5a0e2fce6bdf151b2d5c740cb0446fd4
James Shelton <jshelton140@googlemai.com>
2020-11-03 10:07:54 +0000

    Room JavaDoc

--- src/zuul/Room.java
+++ src/zuul/Room.java
@@ -7,86 +7,124 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
- * Class zuul.Room - a room in an adventure game.
+ * A room in the "World of Zuul" application.
  *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- *
- * A "zuul.Room" represents one location in the scenery of the game.  It is
- * connected to other rooms via exits.  The exits are labelled north, 
- * east, south, west.  For each direction, the room stores a reference
- * to the neighboring room, or null if there is no exit in that direction.
+ * This represents one location in the scenery of the game.
+ * It is connected to other rooms via exits.
+ * For each exit direction, the room stores a reference to the neighboring room.
+ * The room has an inventory that can store items.
+ * The room stores a list of {@link Character Characters} it contains.
  * 
  * @author  Michael Kolling and David J. Barnes
- * @version 2006.03.30
+ * @author Timothy Shelton
  */
 public class Room {
-    private String name;
+    private final String name;
     private String description;
-    
-    // Exits from the room
-    private HashMap<String, Room> exits;
-
-    // An item in the room
-    private Inventory inventory;
-    
-    // Characters in the room
-    private ArrayList<Character> characters;
 
     /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
+     * All exits from this room.
+     *
+     * HashMap ensures all exit directions are unique.
+     */
+    private final HashMap<String, Room> exits;
+
+    /**
+     * An inventory to store items held by this room
+     */
+    private final Inventory inventory;
+
+    /**
+     * A list of each {@link Character} currently in this room.
+     */
+    private final ArrayList<Character> characters;
+
+    /**
+     * Creates a room with name "name" and described "description".
+     * This room does not contain exits, {@link Item Items} or {@link Character Characters}.
+     * <p>
+     * "description" is something like "a kitchen" or "an open court yard".
+     *
+     * @param name  the name of the room, not null
+     * @param description  the room's description, not null
      */
     public Room(String name, String description) {
         this.name = name;
         this.exits = new HashMap<String, Room>();
         this.description = description;
-        this.characters = new ArrayList<>();
+        this.characters = new ArrayList<Character>();
         this.inventory = new Inventory();
     }
 
     /**
-     * Define the exits of this room.  Every direction either leads
-     * to another room or is null (no exit there).
-     * @param directions The set of exit directions.
-     * @param rooms The set of rooms lead to by the exit directions.
+     * Defines the exits of this room.
+     * <p>
+     * Every direction must lead to another room.
+     * Only the first exit will be added for each unique direction name.
+     *
+     * @param directions  the set of exit directions, not null
+     * @param rooms  the set of rooms lead to by the exit directions, not null
      */
     public void setExits(String[] directions, Room[] rooms) {
+
         if (directions.length > rooms.length) {
             //not every exit leads to a room!
+            GameInterface.get().update("room error exitNoRoom");
             zuul.io.Out.println(GameText.getString("exitNoRoomError"));
             return;
         }
+
         for(int i = 0; i < directions.length; i++) {
+            if(rooms[i] == null){
+                //next exit would not lead to a room!
+                GameInterface.get().update("room error exitNoRoom");
+                zuul.io.Out.println(GameText.getString("exitNoRoomError"));
+                return;
+            }
+            //for each direction link the corresponding room
             addExit(directions[i], rooms[i]);
         }
     }
 
     /**
-     * Add an exit to the room.
-     * @param direction The direction of the exit.
-     * @param room The room the exit leads to.
+     * Ensures that the specified exit is part of this Room's state.
+     *
+     * @param direction  the direction of the exit, not null
+     * @param room  the room the exit leads to, not null
      */
     public void addExit(String direction, Room room) {
         this.exits.put(direction, room);
     }
 
     /**
-     * @return The description of the room.
+     * Gets the description of this room.
+     *
+     * @return the description of this room, not null
      */
     public String getDescription() {
         return description;
     }
 
     /**
-     * @return The characters currently in the room.
+     * Gets a shallow copy of the list of each {@link Character} in the room.
+     * <p>
+     * Only a shallow copy is performed so that Characters are not duplicated.
+     *
+     * @return the list characters currently in the room, not null
      */
     public ArrayList<Character> getCharacters() {
         return (ArrayList<Character>) characters.clone();
     }
 
+    /**
+     * Gets the {@link Character} specified by name if it is in this room.
+     * <p>
+     * Always returns
+     *
+     * @param name  the name of the character to find, not null
+     * @return an Optional containing the Character with the name specified if found in this room's state,
+     *          an empty optional otherwise, not null
+     */
     public Optional<Character> getCharacter(String name){
         return characters.stream()
                 .filter(character -> character.getName().equals(name))
@@ -94,7 +132,10 @@ public class Room {
     }
 
     /**
-     * @return the room in direction exit or null if it doesn't exist
+     * Gets the Room in the direction specified if there is an exit in that direction in this room.
+     *
+     * @param exit  the direction of exit from this room to retrieve the corresponding room for
+     * @return an Optional containing the room in direction exit or null if it doesn't exist
      */
 
     public Optional<Room> getExit(String exit) {
@@ -103,11 +144,14 @@ public class Room {
             return Optional.empty();
         }
 
+        //the exit exists so return the room it leads to
         return Optional.of(this.exits.get(exit));
     }
 
     /**
-     * Prints the description, exits, and items contained within this Room.
+     * Prints the state of this room.
+     * <p>
+     * The state of this room includes the description, exits, and {@link Inventory} of this Room.
      */
     public void printInfo() {
         zuul.io.Out.println("You are " + getDescription());
@@ -127,15 +171,17 @@ public class Room {
     }
 
     /**
-     * Add a character to the room.
-     * @param character The character to add.
+     * Ensures that the specified {@link Character} is part of this Room's state.
+     *
+     * @param character the Character to ensure is in this room
      */
     public void addCharacter(Character character) {
         this.characters.add(character);
     }
 
     /**
-     * Remove a character from the room.
+     * Removes a {@link Character} from this room.
+     *
      * @param character The character to remove.
      */
     public void removeCharacter(Character character) {
@@ -143,7 +189,11 @@ public class Room {
     }
 
     /**
-     * @return The character's inventory.
+     * Gets the {@link Inventory} of this Room.
+     * <p>
+     * It is returned directly so that it can be modified without {@link Item} duplication.
+     *
+     * @return this Room's Inventory
      */
     public Inventory getInventory() {
         return inventory;
