ec7bf8d36f7013b9b5b12cdb358f8e266889b3c4
James Shelton <jshelton140@googlemai.com>
2020-11-03 11:34:15 +0000

    CommandFactory JavaDoc

--- src/zuul/CommandFactory.java
+++ src/zuul/CommandFactory.java
@@ -3,20 +3,28 @@ package zuul;
 import java.util.ArrayList;
 import java.util.Optional;
 
+/**
+ * A factory to build {@link Command} objects for the "World of Zuul" application.
+ *
+ * @author Timothy Shelton
+ */
 public class CommandFactory {
 
     /**
-     * Constructor
+     * Constructor does nothing
      */
     public CommandFactory() {
         //do nothing
     }
 
     /**
-     * Create command from commandWord String by building the class name using reflection.
-     * @param commandWord
-     * @param modifiers
-     * @return
+     * Creates a {@link Command} from commandWord String by building the class name using reflection.
+     * <p>
+     * All exceptions thrown are caught and treated as an unrecognised command word and an empty optional is returned.
+     *
+     * @param commandWord  the command word of the Command to create, not null
+     * @param modifiers  the list of modifier words to instantiate the created Command with, not null
+     * @return an Optional containing a Command object if one was successfully instantiated, otherwise an empty Optional
      */
     public Optional<Command> getCommand(String commandWord, ArrayList<String> modifiers) {
         try{
@@ -30,11 +38,19 @@ public class CommandFactory {
                     .getConstructor(new Class<?>[]{ArrayList.class})
                     .newInstance(modifiers));
         } catch (Exception e) {
-            //Catch all exceptions and return an "null command"
+            //Catch all exceptions and return a "no command" optional
             return Optional.empty();
         }
     }
 
+    /**
+     * Gets the commandWord part of the simple class name of the Command specified
+     * by the locale-dependant commandWord supplied.
+     *
+     * @param commandWord  the locale-dependent commandWord to get the Command Class name of, not null
+     * @return  an Optional containing the commandWord part of the simple class name of the command
+     *          corresponding to the supplied command word if it is found, otherwise an empty Optional
+     */
     private Optional<String> getCommandClassName(String commandWord) {
         return GameText.getCommandWords().stream()
                 .filter(key -> GameText.getString("CommandWordsBundle", key).equalsIgnoreCase(commandWord))
